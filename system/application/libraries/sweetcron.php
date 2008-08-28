76a5303f38ec4a6245061eb01b691b4e29e50460
yongfook <yongfook@51b50d7d-0b4b-0410-974b-f188aa86c7d9>
2008-08-28 17:42:24 +0000

    fixed feed subdomain borkage
    git-svn-id: https://sweetcron.googlecode.com/svn/trunk@39 51b50d7d-0b4b-0410-974b-f188aa86c7d9

--- system/application/libraries/sweetcron.php
+++ system/application/libraries/sweetcron.php
@@ -38,7 +38,7 @@ class Sweetcron {
 	
     function __construct() {
     	$this->CI =& get_instance();
-    	$this->CI->config->set_item('sweetcron_version', '1.04');
+    	$this->CI->config->set_item('sweetcron_version', '1.05');
     }
 	
 	function fetch_items()
