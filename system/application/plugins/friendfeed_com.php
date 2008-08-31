8a6f15651e57e81ec0221b711134daeefab9c672
yongfook <yongfook@51b50d7d-0b4b-0410-974b-f188aa86c7d9>
2008-08-31 01:45:24 +0000

    added friendfeed plugin
    git-svn-id: https://sweetcron.googlecode.com/svn/trunk@58 51b50d7d-0b4b-0410-974b-f188aa86c7d9

---
+++ system/application/plugins/friendfeed_com.php
@@ -0,0 +1,30 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Friendfeed_com {
+	
+	//sample class for friendfeed
+
+	function pre_db($item)
+	{
+		return $item;
+	}
+	
+	function pre_display($item)
+	{
+	    $big_pic = $item->item_data['enclosures'][0]->link;
+	    //check if is image
+	    $format = substr($big_pic, -4);
+	    if ($format == '.jpg' || $format == '.gif' || $format == '.png') {
+	    	$item->item_data['image'] = $big_pic;
+	    }
+	    
+	    //remove small icons as image
+	    if (substr($item->item_data['image'], 0, 42) == 'http://friendfeed.com/static/images/icons/') {
+	    	$item->item_data['image'] = '';	
+	    }
+		return $item;
+	}
+
+
+}
+?>
