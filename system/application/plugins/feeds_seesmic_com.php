3b3fb622b5344711f69a9e49cfe4b3e865695084
yongfook <yongfook@51b50d7d-0b4b-0410-974b-f188aa86c7d9>
2008-09-05 08:38:01 +0000

    added plugin for seesmic (adds video)
    git-svn-id: https://sweetcron.googlecode.com/svn/trunk@82 51b50d7d-0b4b-0410-974b-f188aa86c7d9

---
+++ system/application/plugins/feeds_seesmic_com.php
@@ -0,0 +1,28 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Feeds_seesmic_com {
+	
+	//sample class for seesmic
+
+	function pre_db($item, $original)
+	{
+		return $item;
+	}
+	
+	function pre_display($item)
+	{
+	    //seesmic has a "hidden" image...
+	    $item->item_data['image'] = $item->item_data['enclosures'][0]->thumbnails[0];
+	    
+	    //get video id
+	    $videoid = explode('/', $item->item_data['permalink']);
+	    $videoid = $videoid[4];
+
+	    //create teh video
+	    $item->item_data['video'] = '<object width="435" height="355"><param name="movie" value="http://seesmic.com/embeds/wrapper.swf"/><param name="bgcolor" value="#666666"/><param name="allowFullScreen" value="true"/><param name="allowScriptAccess" value="always"/><param name="flashVars" value="video='.$videoid.'&version=threadedplayer"/><embed src="http://seesmic.com/embeds/wrapper.swf" type="application/x-shockwave-flash" flashVars="video='.$videoid.'&version=threadedplayer" allowFullScreen="true" bgcolor="#666666" allowScriptAccess="always" width="435" height="355"></embed></object>';
+		return $item;
+	}
+
+
+}
+?>
