f574ae31471bb1dbeff9bdf81ce08a9f6c7154df
yongfook <yongfook@51b50d7d-0b4b-0410-974b-f188aa86c7d9>
2008-09-06 14:15:18 +0000

    increment version number - 1.08b
    git-svn-id: https://sweetcron.googlecode.com/svn/trunk@85 51b50d7d-0b4b-0410-974b-f188aa86c7d9

--- system/application/libraries/sweetcron.php
+++ system/application/libraries/sweetcron.php
@@ -38,7 +38,7 @@ class Sweetcron {
 	
     function __construct() {
     	$this->CI =& get_instance();
-    	$this->CI->config->set_item('sweetcron_version', '1.08a');
+    	$this->CI->config->set_item('sweetcron_version', '1.08b');
     }
 	
 	function fetch_items()
